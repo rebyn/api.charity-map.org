0e8c2c4d6a19a90e957a2078e524b327e5b06762
Tu Hoang <rebyn@me.com>
2014-01-11 12:13:22 +0700

    g model User email:string

---
+++ spec/factories/users.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :integer          not null, primary key
+#  email      :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+# Read about factories at https://github.com/thoughtbot/factory_girl
+
+FactoryGirl.define do
+  factory :user do
+    email "MyString"
+  end
+end
