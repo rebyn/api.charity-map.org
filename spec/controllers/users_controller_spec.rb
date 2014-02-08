4e76a31d1efacfd94c58a5e7c00da18d311c3d5d
vumanhcuongit <cuongvu0103@gmail.com>
2014-02-08 13:16:50 +0700

    add test for create user without token

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,25 +1,43 @@
 require 'spec_helper'
 
 describe V1::UsersController do
-  before :each do
-    request.env["HTTP_ACCEPT"] = 'application/json'
-    request.env["HTTPS"] = 'on'
-    request.env["HTTP_AUTHORIZATION"] = "Token token=ABCabc"
-  end
+  describe "Action with Auth Token" do
 
-  describe "Create User" do
-    it "should create a user and returning auth token" do
-    	@params = {email: "merchant@company.com", category: "MERCHANT"}
-      post :create, @params
-      expect(response.status).to eq(200)
-      expect(response.body).to have_node(:auth_token)
+    before :each do
+      request.env["HTTP_ACCEPT"] = 'application/json'
+      request.env["HTTPS"] = 'on'
+      request.env["HTTP_AUTHORIZATION"] = "Token token=ABCabc"
     end
 
-    it "should not create a user with invalid params" do
-    	@params = {category: "MERCHANT"}
+    describe "Create User" do
+      it "should create a user and returning auth token" do
+      	@params = {email: "merchant@company.com", category: "MERCHANT"}
+        post :create, @params
+        expect(response.status).to eq(200)
+        expect(response.body).to have_node(:auth_token)
+      end
+
+      it "should not create a user with invalid params" do
+      	@params = {category: "MERCHANT"}
+        post :create, @params
+        expect(response.status).to eq(400)
+        expect(response.body).to eq({:"error" => "Cannot create user"}.to_json)
+      end
+    end
+  end
+
+  describe "Check Auth Token" do
+    before :each do
+      request.env["HTTP_ACCEPT"] = 'application/json'
+      request.env["HTTPS"] = 'on'
+      request.env["HTTP_AUTHORIZATION"] = "Token token=FAKE_TOKEN"
+    end
+
+    it "should not access action with invalid auth token" do
+      @params = {email: "merchant@company.com", category: "MERCHANT"}
       post :create, @params
-      expect(response.status).to eq(400)
-      expect(response.body).to eq({:"error" => "Cannot create user"}.to_json)
+      expect(response.status).to eq(401)      
+      expect(response.body).to eq("HTTP Token: Access denied.\n")
     end
   end
 end
