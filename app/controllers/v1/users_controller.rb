f6af35c05b85cb880f88fe0aa11460b30ffde02f
Tu Hoang <rebyn@me.com>
2014-02-10 23:55:04 +0700

    validates admin_token on User#create

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -6,7 +6,7 @@ module V1
       if params && params[:email] && params[:category]
         @user = User.new(email: params[:email], category: params[:category])
         if @user.save
-          UserMailer.send_auth_token(@user).deliver
+          # UserMailer.send_auth_token(@user).deliver
           render json: { auth_token: @user.auth_token.value }, status: 201
         else
           render json: { error: @user.errors.full_messages.join(" ") }, status: 400
@@ -19,7 +19,7 @@ module V1
     protected
       def authenticate
         authenticate_or_request_with_http_token do |token, options|
-          token == "ABCabc"
+          token == ENV['API_CREATE_USER_TOKEN']
         end
       end
   end
