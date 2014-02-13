7c50053885c883e4ba716298144f513fc27a66ba
Tu Hoang <rebyn@me.com>
2014-02-13 18:45:41 +0700

    show @user on User#create if User exists

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -4,12 +4,17 @@ module V1
 
   	def create
       if params && params[:email] && params[:category]
-        @user = User.new(email: params[:email], category: params[:category])
-        if @user.save
-          # UserMailer.send_auth_token(@user).deliver
-          render json: { auth_token: @user.auth_token.value }, status: 201
+        if @user = User.find_by(email: params[:email])
+          respond_to {|format| format.json {render(template: 'v1/users/show.json.jbuilder',
+            status: 200)}}
         else
-          render json: { error: @user.errors.full_messages.join(" ") }, status: 400
+          @user = User.new(email: params[:email], category: params[:category])
+          if @user.save
+            # UserMailer.send_auth_token(@user).deliver
+            render json: { auth_token: @user.auth_token.value }, status: 201
+          else
+            render json: { error: @user.errors.full_messages.join(" ") }, status: 400
+          end
         end
       else
         render json: { error: "Missing required params" }, status: 400
